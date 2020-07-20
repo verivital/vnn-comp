fixed eps normalisation issues

2020/CNN/oval_framework/tools/bab_tools/bab_runner.py
@@ -169,7 +169,7 @@ def main():
             if not os.path.exists(dom_path):
                 os.makedirs(dom_path)
         else: 
-            path = './cifar_properties/'
+            path = './batch_verification_results/'
             result_path = './cifar_results/'
 
         if not os.path.exists(result_path):
@@ -289,7 +289,7 @@ def main():
             imag_idx = new_idx
             eps_temp = float(args.nn_name[6:]) if args.data == 'mnist' else float(args.nn_name.split('_')[1])/float(args.nn_name.split('_')[2])
 
-            x, verif_layers, test = load_1to1_eth(args.data, args.nn_name, idx=imag_idx, test=tests, eps_temp=eps_temp,
+            x, verif_layers, test, domain = load_1to1_eth(args.data, args.nn_name, idx=imag_idx, test=tests, eps_temp=eps_temp,
                                                   max_solver_batch=args.max_solver_batch)
             if x is None:
                 # handle misclassified images
@@ -297,7 +297,6 @@ def main():
             # since we normalise cifar data set, it is unbounded now
             bounded = False
             prop_idx = test
-            domain = torch.stack([x.squeeze(0) - eps_temp, x.squeeze(0) + eps_temp], dim=-1)
             linear = False
 
         else:

2020/CNN/oval_framework/tools/bab_tools/model_utils.py
@@ -394,9 +394,6 @@ def add_properties(model, true_label, lb_abs = -1000, ret_ls = False, domain=Non
 def normalize(image, dataset):
     mean, sigma = get_mean_sigma(dataset)
     return (image - mean) / sigma
-    for i in range(len(image)):
-        image[i] -= mnist_mean[i]
-        image[i] /= mnist_sigma[i]
 
 class SeqNet(nn.Module):
 
@@ -596,9 +593,20 @@ def load_1to1_eth(dataset, model, idx = None, test = None, mnist_test = None, ep
         # layers = list(model.children())
         # added_prop_layers = add_single_prop(layers, y_pred, test)
         # return x, added_prop_layers, test
-        domain = torch.stack([x.squeeze(0) - eps_temp, x.squeeze(0) + eps_temp], dim=-1)
+        if dataset == 'cifar10':
+            x_m_eps = normalize(torch.from_numpy(np.array(
+                image - eps_temp, dtype=np.float32).reshape([1, 32, 32, 3]).transpose(0, 3, 1, 2)).clamp(0,1), dataset)
+            x_p_eps = normalize(torch.from_numpy(np.array(
+                image + eps_temp, dtype=np.float32).reshape([1, 32, 32, 3]).transpose(0, 3, 1, 2)).clamp(0,1), dataset)
+        elif dataset == 'mnist':
+            x_m_eps = normalize(torch.from_numpy(
+                np.array(image - eps_temp, dtype=np.float32).reshape([1, 1, 28, 28])).clamp(0,1).float(), dataset)
+            x_p_eps = normalize(torch.from_numpy(
+                np.array(image + eps_temp, dtype=np.float32).reshape([1, 1, 28, 28])).clamp(0,1).float(), dataset)
+
+        domain = torch.stack([x_m_eps.squeeze(0), x_p_eps.squeeze(0)], dim=-1)
         added_prop_layers = add_properties(model, y, lb_abs, domain=domain, max_solver_batch=max_solver_batch)
         for layer in added_prop_layers:
             for p in layer.parameters():
                 p.requires_grad = False
-        return x, added_prop_layers, None
+        return x, added_prop_layers, None, domain

2020/CNN/oval_framework/tools/bounding_tools/bounding_runner.py
@@ -81,11 +81,10 @@ def run_lower_bounding():
 
             eps_temp = float(args.nn_name[6:]) if args.data == 'mnist' else float(args.nn_name.split('_')[1])/float(args.nn_name.split('_')[2])
 
-            x, model, test = load_1to1_eth(args.data, args.nn_name, idx=args.prop_idx, test=tests, eps_temp=eps_temp,
+            x, model, test, domain = load_1to1_eth(args.data, args.nn_name, idx=args.prop_idx, test=tests, eps_temp=eps_temp,
                                            max_solver_batch=args.max_solver_batch)
             # since we normalise cifar data set, it is unbounded now
             prop_idx = test
-            domain = torch.stack([x.squeeze(0) - eps_temp, x.squeeze(0) + eps_temp], dim=-1)
 
     lin_approx_string = "" if not args.define_linear_approximation else "-allbounds"
     image = args.prop_idx if args.nn_name else args.img_idx